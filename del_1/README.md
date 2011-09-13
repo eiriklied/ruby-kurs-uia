66fed9e0bd64b0beda06dd54b71c32d2fbfa8657
Kim Joar Bekkelund <kjbekkelund@gmail.com>
2011-09-13 14:02:23 +0200

    Added initial tasks (and examples) for part 1

--- del_1/README.md
+++ del_1/README.md
@@ -2,7 +2,81 @@
 
 TODO: Legge inn
 
-- linker til ressurser
 - pdf fra foredraget
 - oppgaver til studentene
 - løsningsforslag til oppgavene i fasit-branch
+
+## Ressurser
+
+* [Offisiell Ruby-dokumentasjon](http://ruby-doc.org/core/)
+* [Søkbar Ruby-dokumentasjon](http://railsapi.com/doc/ruby-v1.9.2/)
+
+## Oppgaver
+
+### Oppgave 1
+
+Lag en klasse med navnet `Car` som tar imot variablene `brand` og
+`price` når den instansieres.
+
+Hint:
+
+* Metoden som kjøres når en klasse instansieres er `initialize`
+
+### Oppgave 2
+
+Gitt at vi har satt variablen `bmw` slik:
+
+    bmw = Car.new(:bmw, 300_000)
+
+Legg til en metode som gjør at `puts bmw` viser "En bmw koster 300000".
+
+Hint:
+
+* I Java ville man brukt toString
+
+### Oppgave 3
+
+Lag en metode som returnerer `true` dersom prisen er større enn 500 000.
+
+Hint:
+
+* Hvordan skal metoder som returnerer boolean navnsettes i Ruby?
+
+### Oppgave 4
+
+Legg dette i en `.txt`-fil:
+
+    bmw,500000
+    mercedes,550000
+    toyota,340000
+    mini,300000
+    volkswagen,422000
+    porsche,900000
+    lamborghini, 1000000
+    bugatti,12000000
+
+Les ut filen, gå gjennom hver linje, og lag en instans av `Car` for hver
+linje. Legg alle instansene i en array.
+
+Hint:
+
+* Sjekk ut File i Ruby-dok
+* Det finnes en metode i File for å lese ut alle linjene direkte
+
+### Oppgave 5
+
+Hent ut alle instanser fra array-en i forrige utgaver som returerer
+`true` i metoden du lagde i oppgave 3.
+
+### Oppgave 6
+
+Gitt at vi har instanse:
+
+    bmw = Car.new :bmw, 400_000
+    toyota = Car.new :toyota, 300_000
+
+Overload `+` slik at vi kan få ut samlet pris for de to bilene på
+følgende måte:
+
+    bmw + toyota
+    # => 700000
---
+++ del_1/eksempler.rb
@@ -0,0 +1,128 @@
+    "test".class # => String
+    "test".length # => 4
+
+    -1.abs # => 1
+    
+    true.class # => TrueClass
+    
+    v = nil
+    v.nil? # => true
+
+    1 + 1
+    1.+(1)
+    
+    x == 5
+    x.==(5)
+
+    x = "foo"
+    y = "foo"
+    x == y # => true
+    x.equal?(y) # => false
+    
+    x = :foo
+    y = :foo
+    x == y # => true
+    x.equal?(y) # => true
+
+    if condition
+      # do something
+    elsif other_condition
+      # do something else
+    else
+      # do something other
+    end
+    
+    x = 5 if condition
+    # ofte er denne condition en metode som returnerer
+    # true eller false. Da er Ruby-konvensjonen å ha ?
+    # på slutten av metodenavnet.
+
+    unless condition
+      # do something
+    else
+      # do something else
+    end
+    
+    x = 5 unless condition
+
+    [1,2,"test", true].class # => Array
+    { :navn => "Kim" }.class # => Hash
+    (1..10).class            # => Range
+    
+    (1..5).to_a # => [1,2,3,4,5]
+
+    a = [1,2,3]
+    a.each do |value|
+      puts value
+    end
+    # 1
+    # 2
+    # 3
+
+    3.times do
+      puts "Ruby!"
+    end
+    # Ruby!
+    # Ruby!
+    # Ruby!
+
+    a = [1,2,3]
+    a.map do |value|
+      value * 2
+    end
+    # => [2,4,6]
+
+    h = { :x => 1, :y => "test" }
+    h.each do |key, value|
+      puts "#{key} -> #{value}"
+    end
+    # => x -> 1
+    # => y -> test
+
+    def hello_world
+      puts 'Hello UIA!'
+    end
+    hello_world # => Hello UIA!
+
+    def weirdo(x)
+      if x == 5
+        "Bingo"
+      else
+        42
+      end
+    end
+    puts weirdo(5) # => "Bingo"
+    puts weirdo("Ultimate?") # => 42
+
+    def scary!
+      # modifiserer objektet
+    end
+    
+    def raining?
+      # returnerer true eller false
+    end
+
+class MyClass
+  def hello(name)
+    "Hi, #{name}"
+  end
+end
+o = MyClass.new
+puts o.hello("Bob") # => "Hi, Bob"
+
+class MyClass
+  def self.hello(name)
+    "Hi, #{name}"
+  end
+end
+puts MyClass.hello("Bob") # => "Hi, Bob"
+
+a = 1
+@a = 2
+@@a = 3
+$a = 4
+puts a, @a, @@a, $a
+# 1
+# 2
+# 3
+# 4
